bd484a60ffb45a8123640b5d584227184d44a112
Momomartin <martin.dessart@hotmail.fr>
2018-05-09 19:52:53 +0200

    create deck, create basics functions, add images, add if process

--- js/app.js
+++ js/app.js
@@ -2,76 +2,84 @@
  * Create a list that holds all of your cards
  */
  //Infos à choper
-const startButton=document.querySelector('#begin');
-const items=document.getElementsByClassName('card');
+const start_overlay=document.querySelector('#overlay');
 const liste=document.querySelectorAll('.card');
-const contenu=document.querySelector('.container');
 const deck=document.querySelector('.deck');
-
-//lorsqu'on clique sur le boutton de départ
-startButton.addEventListener('click', function(){
-    //console.log(items);
-    //Pour chacun on les remet à zéro
-    for (i=0; i<items.length;i++){
-      items[i].style.background='black';
-      items[i].style.fontSize= '33px';
-    }
-    //Compteur de mouvements
-    const moves=document.querySelector('.moves');
-    moves.textContent='15';
-
-    //On crée un array, on convertit la liste de card et on la shuffle
-    //array=[1,2,3,4,5]
-    var arr = [];
-    for (var i = 0, ref = arr.length = liste.length; i < ref; i++) {
-     arr[i] = liste[i];
-    }
-    shuffle(arr);
-    console.log(arr);
-
-    //Incorporer nouvelles cartes dans HTML
-    for (i=0; i<arr.length; i++) {
-      let nouvelleCard=arr[i];
-      console.log(nouvelleCard);
-      deck.appendChild(nouvelleCard);
-    }
-    //On supprime le deck ou les cartes une part une
-    /*for (i=0; i<liste.length;i++){
-      items[i].remove();
-    }
-    //On crée les nouvelles cartes
-    const newCard=document.createElement('li');
-    newCard.className='card';
-    deck.appendChild(newCard);*/
-
-    //essai de modifier cartes
-    for (i=0; i<arr.length ;i++){
-      const carte=arr[i];
-      carte.addEventListener('click', function(){
-        carte.style.background='red';
-        carte.style.fontSize= '20px';
-        var selectedCard = [];
-        var openCard=selectedCard.push(carte);
-        console.log('this is the final countdown');
-        console.log(openCard);
-      });
-    }
+const moves=document.querySelector('.moves');
+let arr = [];
+let selectedCard = [];
+let openCard=[];
+let openProp=[];
 
 
+
+//Waiting for the first click
+start_overlay.addEventListener('click', function(){
+  startin();
+  //Shuffle the array
+  shuffle(arr);
+  deckPuttin();
 })
 
+//On the game
+deck.addEventListener('click', function(event){
+  if (event.target.nodeName === 'DIV') {
+    let image=event.target.children[0];
+    console.log(image);
+    image.classList.add("flip");
+  let parentCard=event.target.parentNode;
+  event.target.classList.add("frontflip");
+  //visib=parentCard.children[1].children[0];
+  //console.log(visib);
+  //visib.style.visibility= "visible";
+  console.log(parentCard);
+  altCard=parentCard.children[0].alt;
+  cardProp=parentCard.parentNode;
+  openProp.push(cardProp);
+  openCard.push(altCard);
+  console.log(openCard);
+        if (openCard.length !== 'undefined' && openCard.length > 1) {
+            if (openCard[0]===openCard[1]) {
+              console.log(cardProp);
+              openProp[0].classList="card match cardwin";
+              openProp[1].className="card match cardwin";
+              openProp=[];
+              openCard=[];
+            }
+            else {
+              openProp[0].className="card front backflip";
+              openProp[1].className="card front backflip";
+              openProp=[];
+              openCard=[];
+            }
+          }
+          else {
+            cardProp.className="card open";
+          }
+      }});
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+//FUNCTION STARTIN
+function startin(){
+  //Define the cards style
+  const image=document.getElementsByTagName('img');
+  let arrImage = Array.from(image);
 
-// Shuffle function from http://stackoverflow.com/a/2450976
+  for (i=0;i<arrImage.length;i++){
+    //arrImage[i].style.visibility="visible";
+    //arrImage[i].className="hiddin";
+    console.log(arrImage[i]);
+  }
+  //Movement counter
+  moves.textContent='15';
+  //Get every card into an array
+  for (let i = 0, ref = arr.length = liste.length; i < ref; i++) {
+   arr[i] = liste[i];
+  }
+}
+
+// FUNCTION SHUFFLE from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
+    let currentIndex = array.length, temporaryValue, randomIndex;
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
@@ -79,18 +87,17 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-
     return array;
 }
 
-
-/*const cardOne=document.querySelectorAll('.card');
-//cardOne.addEventListener('click', function(){
-cardOne.addEventListener('click', function(){
-  cardOne.style.background='red';
-  cardOne.style.fontSize= '20px';
-})
-*/
+//FUNCTION DECK PUTTING
+function deckPuttin(){
+  //Add cards on the HTML
+  for (i=0; i<arr.length; i++) {
+    let nouvelleCard=arr[i];
+    deck.appendChild(nouvelleCard);
+  }
+}
 
 
 /*
@@ -103,3 +110,11 @@ cardOne.addEventListener('click', function(){
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+ // FUNCTION OVERLAY
+ function on() {
+     document.getElementById("overlay").style.display = "block";
+ }
+ function off() {
+     document.getElementById("overlay").style.display = "none";
+ }
